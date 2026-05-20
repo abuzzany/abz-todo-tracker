caf91bed8d7bcd354aa4bd9d6fc64ae744ec6224
Angel Buzany <abuzzany@gmail.com>
2026-05-19 18:09:54 -0600

    chore: generate ToDoItem controller

---
+++ app/controllers/to_do_item_controller.rb
@@ -0,0 +1,2 @@
+class ToDoItemController < ApplicationController
+end
---
+++ test/controllers/to_do_item_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ToDoItemControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

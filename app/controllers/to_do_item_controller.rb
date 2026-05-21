3e963f23e3c3ea5cc8dfb17fb2d0c783302e17c7
Angel Buzany <abuzzany@gmail.com>
2026-05-21 11:27:09 -0600

    refactor: replace ToDoItemController with ToDoItemsController and add create action

--- app/controllers/to_do_item_controller.rb
+++
@@ -1,2 +0,0 @@
-class ToDoItemController < ApplicationController
-end
--- test/controllers/to_do_item_controller_test.rb
+++ test/controllers/to_do_item_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ToDoItemControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create saves a to-do item" do
+    assert_difference "ToDoItem.count", 1 do
+      post to_do_items_path, params: {
+        to_do_item: { title: "Learn controllers", description: "Integration test" }
+      }
+    end
+    assert_response :created  # or :redirect if you redirect instead of head :created
+    item = ToDoItem.last
+    assert_equal "Learn controllers", item.title
+  end
+  test "create with invalid data does not save" do
+    # once you add validations, e.g. validates :title, presence: true
+    assert_no_difference "ToDoItem.count" do
+      post to_do_items_path, params: { to_do_item: { title: "" } }
+    end
+    assert_response :unprocessable_entity
+  end
 end

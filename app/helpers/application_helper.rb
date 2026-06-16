7706e4fb9179af127b96f4c1db534f0ed56818c4
Angel Buzany <abuzzany@gmail.com>
2026-06-16 12:09:48 -0600

    feat: create helper to encapsulate the style of the prymari buttons and reuse it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def primary_button_to(text, url, options = {})
+    link_to text, url, options.merge(class: "rounded bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 #{options[:class]}")
+  end
 end
